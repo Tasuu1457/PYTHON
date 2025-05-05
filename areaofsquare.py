4bb231b6011bfab4e8847ff02af0060551d2f3ad
Tasfiha Tahsin <tasfiatoa5@gmail.com>
2025-05-06 00:35:03 +0600

    Area of square, trapezium & rectangle

---
+++ areaofsquare.py
@@ -0,0 +1,3 @@
+side = float(input("Enter the side of the square: "))
+area = side*side
+print("The area of square is ", area)
---
+++ areaoftrapezium.py
@@ -0,0 +1,5 @@
+a = float(input("Enter the base of the trapezium: "))
+b = float(input("Enter another base of the trapezium: "))
+height = float(input("Enter the height of the trapezium: "))
+area = (1/2)*(a+b)*height
+print("The area of trapezium is ", area)
