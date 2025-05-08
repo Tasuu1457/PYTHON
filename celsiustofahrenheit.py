c967d0f02ad19f79b4be066f838e4c6915e5716a
Tasfiha Tahsin <tasfiatoa5@gmail.com>
2025-05-08 22:35:22 +0600

    F and C

---
+++ celsiustofahrenheit.py
@@ -0,0 +1,3 @@
+C = float(input("Enter Celsius: "))
+F = (C*9/5)+32
+print("The fahrenheit is ", F)
---
+++ fahrenheittocelsius.py
@@ -0,0 +1,3 @@
+F = float(input("Enter Fahrenheit: "))
+C = (F-32)*5/9
+print("The Celsius is ", C)
