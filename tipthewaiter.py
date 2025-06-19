9fa70a98d549a4e95c91913c9bc5f926dac8a393
Tasfiha Tahsin <tasfiatoa5@gmail.com>
2025-06-19 22:45:31 +0600

    Arguements

---
+++ tipthewaiter.py
@@ -0,0 +1,16 @@
+def calculate_bill(subtotal, tip_percent, tax_percent):
+    tip_amount = subtotal * (tip_percent/100)
+    tax_amount = subtotal * (tax_percent/100)
+    total = subtotal + tip_amount + tax_amount
+    
+    print("\n--- Restaurant Bill ---")
+    print(f"Subtotal: ${subtotal: 0.2f}")
+    print(f"Tip ({tip_amount}%): ${tip_amount: 0.2f}")
+    print(f"Tax ({tax_amount}%): ${tax_amount: 0.2f}")
+    print(f"Total: ${total: 0.2f}")
+     
+total = float(input("Enter the subtotal amount: $"))
+tip = float(input("Enter the tip percentage: "))
+tax = 8
+
+calculate_bill(total, tip, tax)
